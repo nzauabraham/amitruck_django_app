4d4e8ec0e669e95a4319d067bcb5fa1912eaf97e
nzau <abrahamnzau@gmail.com>
2022-09-08 12:30:20 +0300

    Implement serialization of response

--- main/serializers.py
+++ main/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
-from .models import Trip
 
+class TripSerializer(serializers.Serializer):
+    driver_id = serializers.IntegerField()
+    vehicle_id = serializers.IntegerField()
+    customer_id = serializers.IntegerField()
+    address = serializers.CharField()
+    cargo_tonnage = serializers.DecimalField(max_digits=10, decimal_places=2)
+    address_type = serializers.CharField()
+    done_by_user_id = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
 
-class TripSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Trip
-        fields = (
-        'driver_id', 'vehicle_id', 'customer_id', 'address', 'cargo_tonnage', 'address_type', 'done_by_user_id',
-        'created_at', 'updated_at')
--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Trip
+from .serializers import TripSerializer
 
 
 class TripView(APIView):
     def get(self, request):
         trip_logs = Trip.objects.all()
-        return Response({"trip_logs": trip_logs})
+        serializer = TripSerializer(trip_logs, many=True)
+        return Response({"trip_logs": serializer.data})
